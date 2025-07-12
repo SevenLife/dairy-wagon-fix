b2d6f97a28578f579de1327e5735bb3d1dda8419
SevenLife <cfmpsevenlife@gmail.com>
2025-07-12 02:09:24 +0200

    Add files via upload
    Just drag and drop them into a stream folder , enjoy !

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version "adamant"
+
+game "rdr3"
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+
+author "Sevenlife"
+description 'Simple fix for the dairy wagon to be able to climb inside on the back'
+version '1.0.0'
